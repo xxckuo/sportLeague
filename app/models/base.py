269f5fe4dc44ac853604974c18dfcdb57de9ec68
Dongzhujie <D_zhujie@163.com>
2019-04-12 12:06:40 +0800

    修改管理学校controller

--- app/models/base.py
+++ app/models/base.py
@@ -1,6 +1,7 @@
 """
  Created by 七月 on 2018/5/11.
 """
+from app.libs.error_code import NotFound
 
 __author__ = '七月'
 
@@ -30,6 +31,18 @@ class Query(BaseQuery):
         if 'status' not in kwargs.keys():
             kwargs['status'] = 1
         return super(Query, self).filter_by(**kwargs)
+
+    def get_or_404(self, ident):
+        rv = self.get(ident)
+        if not rv:
+            raise NotFound()
+        return rv
+
+    def first_or_404(self):
+        rv = self.first()
+        if not rv:
+            raise NotFound()
+        return rv
 
 
 db = SQLAlchemy(query_class=Query)

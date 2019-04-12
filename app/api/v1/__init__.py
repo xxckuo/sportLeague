db0b9491f5c5a5190c3ca8792cf28506a703c19b
mei <2931195032@qq.com>
2019-04-12 21:53:09 +0800

    上传

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,11 +1,11 @@
 from flask import Blueprint
-from app.api.v1 import user, book, client, school
+from app.api.v1 import school_admin,school,category
+
 
 def create_blueprint_v1():
     bp_v1 = Blueprint('v1', __name__)
-    user.api.register(bp_v1)
-    book.api.register(bp_v1)
-    client.api.register(bp_v1)
+    school_admin.api.register(bp_v1)
     school.api.register(bp_v1)
+    category.api.register(bp_v1)
 
     return bp_v1

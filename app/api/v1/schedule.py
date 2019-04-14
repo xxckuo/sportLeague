15269d4111983299245dc5a0c4d591323388b062
mei <2931195032@qq.com>
2019-04-14 15:04:48 +0800

    上传

--- app/api/v1/schedule.py
+++ app/api/v1/schedule.py
@@ -1,15 +1,18 @@
 from flask import request
+from sqlalchemy import func
 
 from app.libs.redprint import Redprint
 from app.models.base import db
 from app.libs.error_code import Success
 from app.models.schedule import Schedule
+from app.models.team import Team
 
 api = Redprint('schedule')
 
 @api.route('/create', methods=['POST'])
 def schedule_create():
     jsonData = request.get_json()
+
     with db.auto_commit():
         schedule =Schedule()
         schedule.league_id = jsonData['league_id']
@@ -28,15 +31,93 @@ def schedule_create():
         db.session.add(schedule)
     return Success(msg='新增赛事成功')
 
-@api.route('/select', methods=['GET'])
+# @api.route('/select', methods=['POST'])
+# def schedule_select():
+#     jsonData = request.get_json()
+#     schedules = []
+#     schedule = Schedule.query.filter_by(status=jsonData['status'],
+#                     league_id=jsonData['league_id'])\
+#         .order_by(Schedule.schedule_time)\
+#         # .offset(jsonData['offset']).limit(jsonData['limit'])
+#
+#     for scs in schedule:
+#         time = [scs.schedule_time]
+#         time2=list(set(time))
+#
+#         print(time2)
+#
+#         group_team_a = db.session.query(Team.team_name, Team.team_logo). \
+#                 filter(Team.team_id == scs.schedule_team_a)
+#         group_team_b = db.session.query(Team.team_name, Team.team_logo). \
+#             filter(Team.team_id == scs.schedule_team_b)
+#
+#         schedule_team = scs.to_json()
+#
+#         for team_b in group_team_b:
+#             teamb = {}
+#             teamb['teamb_name'] = team_b[0]
+#             teamb['teamb_logo'] = team_b[1]
+#             schedule_team['teamb_name']=team_b[0]
+#             schedule_team['teamb_logo']=team_b[1]
+#
+#         for team_a in group_team_a:
+#             teama = {}
+#             teama['teama_name'] = team_a[0]
+#             teama['teama_logo'] = team_a[1]
+#             schedule_team['teama_name'] = team_a[0]
+#             schedule_team['teama_logo'] = team_a[1]
+#         schedules.append(schedule_team)
+#     return Success(msg='查找成功', data=schedules)
+
+@api.route('/select', methods=['POST'])
 def schedule_select():
-    # 根据league_id查找联赛或者杯赛
-    league_id = request.args.get('league_id')
-    schedule = Schedule.query.group_by('schedule_time').all()
-    for sc in schedule:
-        print(sc)
-    # schedules = []
-    # for sc in schedule:
-    #     schedules.append(sc.to_json())
-    # return Success(msg='查找成功', data=schedules)
-    return 'hello'
+
+    jsonData = request.get_json()
+    sc = db.session.query(func.FROM_UNIXTIME(Schedule.schedule_time,'%Y-%m-%d')).filter().group_by(
+        func.FROM_UNIXTIME(Schedule.schedule_time,'%Y-%m-%d')).offset(jsonData['offset']).all()
+    datas = {}
+    for ssc in sc:
+
+        ga = db.session.query(Schedule.schedule_id,Schedule.status,Schedule.league_id,Schedule.schedule_process,
+                              Schedule.schedule_team_a,Schedule.schedule_team_b,Schedule.schedule_support_a,
+                              Schedule.schedule_support_b,Schedule.schedule_score_a,Schedule.schedule_score_b,
+                              Schedule.schedule_location,Schedule.schedule_judge,Schedule.schedule_status,
+                              Schedule.schedule_turn_name)\
+            .filter(func.FROM_UNIXTIME(Schedule.schedule_time,'%Y-%m-%d')==ssc[0]
+                    ,Schedule.league_id==jsonData['league_id']
+                    ).limit(jsonData['limit']).all()
+        games=[]
+        for g in ga:
+            temp = {}
+            temp['schedule_id'] = g[0]
+            temp['league_status'] = g[1]
+            temp['league_id'] = g[2]
+            temp['schedule_process'] = g[3]
+            temp['schedule_team_a'] = g[4]
+            temp['schedule_team_b'] = g[5]
+            temp['schedule_support_a'] = g[6]
+            temp['schedule_support_b'] = g[7]
+            temp['schedule_score_a'] = g[8]
+            temp['schedule_score_b'] = g[9]
+            temp['schedule_location'] = g[10]
+            temp['schedule_judge'] = g[11]
+            temp['schedule_status'] = g[12]
+            temp['schedule_turn_name'] = g[13]
+            games.append(temp)
+
+            group_team_a = db.session.query(Team.team_name, Team.team_logo). \
+                filter(Team.team_id == g[4])
+            group_team_b = db.session.query(Team.team_name, Team.team_logo). \
+                filter(Team.team_id == g[5])
+
+            for team_a in group_team_a:
+                temp['teama_name'] = team_a[0]
+                temp['teama_logo'] = team_a[1]
+            for team_b in group_team_b:
+                temp['teamb_name'] = team_b[0]
+                temp['teamb_logo'] = team_b[1]
+
+        datas[ssc[0]] = games
+
+    return Success(msg='查找成功',data=datas)
+

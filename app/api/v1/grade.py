8288f24d3b3d560928b9c56acd38b1bbcca1f548
lxh666666 <37371011+lxh666666@users.noreply.github.com>
2019-04-15 20:54:02 +0800

    查看积分榜

--- app/api/v1/grade.py
+++ app/api/v1/grade.py
@@ -9,30 +9,22 @@ api = Redprint('grade')
 @api.route('/display_grade')
 def display_grade():
     league_id = request.args.get('league_id')
-    data = db.session.query(Grade).filter(Grade.league_id == league_id)
-    for d in data:
-        team = db.session.query(Grade.grade_id,Grade.grade_team,Grade.grade_turns,Grade.grade_win,Grade.grade_equal
-                                ,Grade.grade_lose,Grade.grade_goal,Grade.grade_fumble,Grade.grade_integral,Team.team_logo,Team.team_name).\
-        filter(Grade.league_id == league_id,Team.team_id == d.grade_team). \
+    data = db.session.query(Grade.grade_id,Grade.grade_team,Grade.grade_turns,Grade.grade_win,Grade.grade_equal
+                                ,Grade.grade_lose,Grade.grade_goal,Grade.grade_fumble,Grade.grade_integral,Team.team_logo,Team.team_name,Team.team_id).filter(Grade.league_id == league_id,Team.team_id == Grade.grade_team).\
             order_by(Grade.grade_integral.desc())
-        tempdata = d.to_json()
-        teams = []
-        for b in team:
-            d={}
-            d['grade_id'] = b[0]
-            d['grade_team'] = b[1]
-            d['grade_turns'] = b[2]
-            d['grade_win'] = b[3]
-            d['grade_equal'] = b[4]
-            d['grade_lose'] = b[5]
-            d['grade_goal'] = b[6]
-            d['grade_fumble'] = b[7]
-            d['grade_integral'] = b[8]
-            d['team_logo']=b[9]
-            d['team_name']=b[10]
-            teams.append(d)
-            tempdata['grade_team'] = teams
-        return Success(msg='查询成功', data=teams)
-
-
-
+    messages = []
+    for t in data:
+        d = {}
+        d['grade_team'] = t[1]
+        d['grade_turns'] = t[2]
+        d['grade_win'] = t[3]
+        d['grade_equal'] = t[4]
+        d['grade_lose'] = t[5]
+        d['grade_goal'] = t[6]
+        d['grade_fumble'] = t[7]
+        d['grade_integral'] = t[8]
+        d['team_logo']=t[9]
+        d['team_name']=t[10]
+        d['team_id']=t[11]
+        messages.append(d)
+    return Success(msg='查询成功', data=messages)

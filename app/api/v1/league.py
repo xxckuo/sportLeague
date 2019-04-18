1ef66a3190673b0f399ead42a1cc641d4f22c43a
lxh666666 <37371011+lxh666666@users.noreply.github.com>
2019-04-18 15:21:28 +0800

    初始化信息判断

--- app/api/v1/league.py
+++ app/api/v1/league.py
@@ -2,7 +2,6 @@ from flask import jsonify, request
 from app.libs.redprint import Redprint
 from app.models.base import db
 from app.libs.error_code import Success
-from app.models.category import Category
 from app.models.grade import Grade
 from app.models.league import League
 from app.models.team import Team
@@ -21,22 +20,25 @@ def create_league():
         league.category_id = jsonData['category_id']
         league.school_id = jsonData['school_id']
         db.session.add(league)
+        #插入赛事的时候顺遍做一哈初始化，将积分榜里面插入的联赛成绩全部设为0
         if int(league.league_type) == 1:
-            team_message = db.session.query(Team.team_id).filter(Team.school_id == jsonData['school_id']).all()
+            team_message = db.session.query(Team.team_id,Team.status).filter(Team.school_id == jsonData['school_id']).all()
             db.session.flush()
             for me in team_message:
-                grade = Grade()
-                grade.grade_team = me[0]
-                grade.league_id = league.league_id
-                grade.grade_turns = 0
-                grade.grade_win = 0
-                grade.grade_equal = 0
-                grade.grade_lose = 0
-                grade.grade_goal = 0
-                grade.grade_fumble =0
-                grade.grade_integral = 0
-                db.session.add(grade)
-    return Success(msg='新增成功')
+                if me[1] == 1:
+                    grade = Grade()
+                    grade.grade_team = me[0]
+                    #0是1，3，4就是查询出来的符合条件的队伍id
+                    grade.league_id = league.league_id
+                    grade.grade_turns = 0
+                    grade.grade_win = 0
+                    grade.grade_equal = 0
+                    grade.grade_lose = 0
+                    grade.grade_goal = 0
+                    grade.grade_fumble =0
+                    grade.grade_integral = 0
+                    db.session.add(grade)
+        return Success(msg='新增成功')
 
 
 @api.route('/query_id')
@@ -68,7 +70,7 @@ def delete_league():
     return Success(msg='删除成功')
 
 
-@api.route('/update_league')
+@api.route('/update_league',methods=['POST'])
 def update_league():
     jsonData = request.get_json()
     with db.auto_commit():

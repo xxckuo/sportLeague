0fae4914dc4ca54def50b928868a54b8a3a03070
meimei <2931195032@qq.com>
2019-04-22 11:31:16 +0800

    球员统计

--- app/api/v1/play_statistics.py
+++ app/api/v1/play_statistics.py
@@ -32,42 +32,44 @@ def play_statistics_create():
 def play_select():
     jsonData = request.get_json()
     plays = []
-    print(jsonData['league_id'])
+
     if int(jsonData['select']) == 1:
         # play = Player_statistics.query.order_by(desc(Player_statistics.pt_score)).all()
-        play = db.session.query(Player_statistics.player_id,Player.player_name,Player_statistics.pt_id,Player_statistics.team_name,Player_statistics.pt_score,Player_statistics.pt_assist,Player_statistics.pt_foul,Player_statistics.pt_yellow_card,Player_statistics.pt_red_card).\
-            filter(Player_statistics.player_id == Player.player_id,Player_statistics.league_id == jsonData['league_id']).\
+        play = db.session.query(Player_statistics.player_id,Player.player_name,Player_statistics.pt_id,Player_statistics.team_name,
+                                Player_statistics.pt_score,Player_statistics.pt_assist,Player_statistics.pt_foul,
+                                Player_statistics.pt_yellow_card,Player_statistics.pt_red_card,Player.player_number).\
+            filter(Player_statistics.pt_score!=0,Player_statistics.player_id == Player.player_id,Player_statistics.league_id == jsonData['league_id']).\
             order_by(Player_statistics.pt_score.desc()).all()
     if int(jsonData['select']) == 2:
-        play = db.session.query(Player_statistics.player_id, Player.player_name, Player_statistics.pt_id,
+        play = db.session.query(Player_statistics.player_id, Player.player_name,Player_statistics.pt_id,
                                 Player_statistics.team_name, Player_statistics.pt_score, Player_statistics.pt_assist,
                                 Player_statistics.pt_foul, Player_statistics.pt_yellow_card,
-                                Player_statistics.pt_red_card). \
-            filter(Player_statistics.player_id == Player.player_id,
+                                Player_statistics.pt_red_card,Player.player_number). \
+            filter(Player_statistics.pt_assist!=0,Player_statistics.player_id == Player.player_id,
                    Player_statistics.league_id == jsonData['league_id']). \
             order_by(Player_statistics.pt_assist.desc()).all()
     if int(jsonData['select']) == 3:
-        play = db.session.query(Player_statistics.player_id, Player.player_name, Player_statistics.pt_id,
+        play = db.session.query(Player_statistics.player_id, Player.player_name,Player_statistics.pt_id,
                                 Player_statistics.team_name, Player_statistics.pt_score, Player_statistics.pt_assist,
                                 Player_statistics.pt_foul, Player_statistics.pt_yellow_card,
-                                Player_statistics.pt_red_card). \
-            filter(Player_statistics.player_id == Player.player_id,
+                                Player_statistics.pt_red_card,Player.player_number). \
+            filter(Player_statistics.pt_foul!=0,Player_statistics.player_id == Player.player_id,
                    Player_statistics.league_id == jsonData['league_id']). \
             order_by(Player_statistics.pt_foul.desc()).all()
     if int(jsonData['select']) == 4:
         play = db.session.query(Player_statistics.player_id, Player.player_name, Player_statistics.pt_id,
                                 Player_statistics.team_name, Player_statistics.pt_score, Player_statistics.pt_assist,
                                 Player_statistics.pt_foul, Player_statistics.pt_yellow_card,
-                                Player_statistics.pt_red_card). \
-            filter(Player_statistics.player_id == Player.player_id,
+                                Player_statistics.pt_red_card,Player.player_number). \
+            filter(Player_statistics.pt_yellow_card!=0,Player_statistics.player_id == Player.player_id,
                    Player_statistics.league_id == jsonData['league_id']). \
             order_by(Player_statistics.pt_yellow_card.desc()).all()
     if int(jsonData['select']) == 5:
-        play = db.session.query(Player_statistics.player_id, Player.player_name, Player_statistics.pt_id,
+        play = db.session.query(Player_statistics.player_id, Player.player_name,Player_statistics.pt_id,
                                 Player_statistics.team_name, Player_statistics.pt_score, Player_statistics.pt_assist,
                                 Player_statistics.pt_foul, Player_statistics.pt_yellow_card,
-                                Player_statistics.pt_red_card). \
-            filter(Player_statistics.player_id == Player.player_id,
+                                Player_statistics.pt_red_card,Player.player_number). \
+            filter(Player_statistics.pt_red_card!=0,Player_statistics.player_id == Player.player_id,
                    Player_statistics.league_id == jsonData['league_id']). \
             order_by(Player_statistics.pt_red_card.desc()).all()
 
@@ -75,6 +77,7 @@ def play_select():
         player = {}
         player['player_id'] = p[0]
         player['player_name'] = p[1]
+        # player['player_number'] = p[2]
         player['pt_id'] = p[2]
         player['team_name'] = p[3]
         player['pt_score'] = p[4]
@@ -82,6 +85,7 @@ def play_select():
         player['pt_foul'] = p[6]
         player['pt_yellow_card'] = p[7]
         player['pt_red_card'] = p[8]
+        player['player_number'] = p[9]
         plays.append(player)
 
     return Success(msg='查找球员积分榜成功', data=plays)

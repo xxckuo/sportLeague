9bb1ca653d771f0dc26a64aa045f36ccc7fd4a1f
meimei <2931195032@qq.com>
2019-04-29 10:22:36 +0800

    返回登录后管理员信息

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -1,11 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, g
 
+from app.api.v1.token_auth import auth
+from app.libs.error_code import Success
 from app.libs.redprint import Redprint
-
-from app.models.school import School
+from app.models.school_admin import School_admin
 
 api = Redprint('user')
 
-@api.route('/get')
+@api.route('/get',methods = ['GET'])
+@auth.login_required
 def get_user():
-    return 'this is project'
+    # uid = g.user.uid
+    # user = School_admin.query.filter_by(sa_id = uid).first()
+    # print (user.school_id)
+    sc = {'uid':g.user.uid,
+        'school_id':g.user.school_id,
+        'admin_name':g.user.admin_name,
+        'sa_auth':g.user.sa_auth
+          }
+
+    schools = []
+
+    schools.append(sc)
+    return Success(msg='查找管理员成功', data=schools)
